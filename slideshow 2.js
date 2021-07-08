e6597152dff15184e6d004e1f665895f73275fd3
panca55 <86353094+panca55@users.noreply.github.com>
2021-07-08 12:07:56 +0700

    Add files via upload

---
+++ slideshow 2.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper('.swiper-container',{cssMode:true,spaceBetween:0,centeredSlides:true,scrollbar:{el:'.swiper-scrollbar',hide:true,},autoplay:{delay:2500,disableOnInteraction:true,},pagination:{el:'.swiper-pagination',dynamicBullets:true,},loop:true,});
